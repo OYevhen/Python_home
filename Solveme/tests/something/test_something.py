7fe699f65525140c0c0a6adf68fcb86baf5d7817
Az Debian <orel.yevhen@gmail.com>
2026-01-22 20:38:14 +0200

    22.01

--- Solveme/tests/something/test_something.py
+++ Solveme/tests/something/test_something.py
@@ -10,3 +10,13 @@ import pytest
 ])
 def test_something(status, get_player_generator):
     print(get_player_generator.build())
+
+
+@pytest.mark.parametrize('balance_value', [
+    '100',
+    '0',
+    '-10',
+    'vbvbv'
+])
+def test_something(balance_value, get_player_generator):
+    print(get_player_generator.set_balance(balance_value).build())
